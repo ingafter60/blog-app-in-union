beb626f99bcbcc915fe8c10e92107eed8093287b
ing <ingafter60@gmail.com>
2020-12-25 10:12:48 +0700

    6. Admin panel - modify display admin panel for categoria and autor

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Categoria)
-admin.site.register(Autor)
+"""
+Modify admin panel display
+1. Add search field - The value of 'search_fields' must be a list or tuple.
+2. Add list_display - The value of 'list_display' must be a list or tuple.
+"""
+class CategoriaAdmin(admin.ModelAdmin):
+	search_fields = ['nomber']
+	list_display  = ('nombre', 'fecha_creacion',  'estado')
+
+class AutorAdmin(admin.ModelAdmin):
+	search_fields = ['nombres', 'apellidos', 'correo']
+	list_display  = ('nombres', 'estado', 'correo', 'fecha_creacion', 'estado')	
+
+# Register models and admin-class
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Autor, AutorAdmin)
